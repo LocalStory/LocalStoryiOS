9095278d61589a06fe83f29642c8da2a995a7bf5
jakehawken <jacobdhawken@gmail.com>
2014-11-14 15:10:34 -0800

    Initial Commit

---
+++ LocalStory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalStory
+//
+//  Created by Jacob Hawken on 11/14/14.
+//  Copyright (c) 2014 Jacob Hawken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
