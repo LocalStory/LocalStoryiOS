8d6b01a24cd304df0217069f2f5df98909f9d08b
Randall Leung <leungran@gmail.com>
2014-11-17 10:08:24 -0800

    Initial Commit

---
+++ LocalStory/StoryViewController.h
@@ -0,0 +1,13 @@
+//
+//  StoryViewController.h
+//  LocalStory
+//
+//  Created by Randall Leung on 11/17/14.
+//  Copyright (c) 2014 Jacob Hawken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StoryViewController : UIViewController
+
+@end
