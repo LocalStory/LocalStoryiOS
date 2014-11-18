3d3564d83b54f864fcf5d853f124f4093c79aaab
Randall Leung <leungran@gmail.com>
2014-11-18 15:38:43 -0800

    initial commit

---
+++ LocalStory/ViewingStoryViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewingStoryViewController.h
+//  LocalStory
+//
+//  Created by Randall Leung on 11/18/14.
+//  Copyright (c) 2014 Jacob Hawken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewingStoryViewController : UIViewController
+
+@end
