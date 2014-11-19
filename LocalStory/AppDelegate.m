5e6f0a995493f8b463d12ef7a85c097e868ff031
Randall Leung <leungran@gmail.com>
2014-11-19 10:49:50 -0800

    add views

--- LocalStory/AppDelegate.m
+++ LocalStory/AppDelegate.m
@@ -16,6 +16,12 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    //Temporary onload - START
+    //self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.loginVC = [[LogInViewController alloc] initWithNibName:@"LogInViewController" bundle:nil];
+    self.window.rootViewController = self.loginVC;
+    //END
   
   return YES;
 }
---
+++ LogInViewController.h
@@ -0,0 +1,13 @@
+//
+//  LogInViewController.h
+//  LocalStory
+//
+//  Created by Randall Leung on 11/19/14.
+//  Copyright (c) 2014 Jacob Hawken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LogInViewController : UIViewController
+
+@end
